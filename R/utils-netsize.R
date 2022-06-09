272090f26f011c6fd4a28d904f48e82dc71b5a2d
UddieEpi <uonwubi@emory.edu>
2022-06-09 10:32:58 -0400

    Create utils-netsize.R

---
+++ R/utils-netsize.R
@@ -0,0 +1,13 @@
+##
+##  Shared variables used all over the project
+##
+##  The variables should be changed here only to avoid discrepancies in the
+##  different scripts
+##
+
+# Size of network to use
+if (! exists("NETSIZE")) {
+  NETSIZE <- 10 * 1e3
+}
+
+netsize_string <- format(NETSIZE, scientific = FALSE)
