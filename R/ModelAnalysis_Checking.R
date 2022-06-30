869426bc5b5c38ecac8b4c9d35de9d0b74592595
UddieEpi <uonwubi@emory.edu>
2022-06-29 23:34:44 -0400

    Update ModelAnalysis_Checking.R

--- R/ModelAnalysis_Checking.R
+++ R/ModelAnalysis_Checking.R
@@ -23,40 +23,53 @@
     
     library(dplyr)
     library(tidyverse)
-    #Tested for HIV B4 PS
-    p_tb4PS<-df.allsims %>% filter(time<1*52) %>% group_by(sim) %>% 
+    
+    #Testing by year
+    #p_tyr<-df.allsims %>% mutate(yr=cut(time,breaks = c(seq(0,10*52,by=52)), labels = c("1","2","3","4","5","6","7","8","9","10"),include.lowest = T)) %>% group_by(yr) %>% 
+  
+    p_tyrND<-as.data.frame(df.allsims) %>% mutate(yr=cut(time,breaks = c(seq(0,4*52,by=52)), labels = c("1","2","3","4"),include.lowest = T)) %>% group_by(yr) %>% 
       summarise(totpop=1000, 
-                incidHIV=sum(incid, na.rm = T), 
-                ndindexes=sum(recent_diagn, na.rm = T), 
-                ndindexes.elig=sum(elig_indexes,na.rm = T), 
-                ndindexes.initPS=sum(found_indexes, na.rm = T),
-                ndpart.elicit=sum(elig_partners, na.rm = T), 
-                ndpart.ident=sum(found_partners, na.rm = T), 
-                ndpart.ident2=sum(tot.part.ident, na.rm=T), 
-                ndpart.eligTst=sum(elig.part.for.pbt, na.rm = T),
-                ndpart.tested=sum(tot.tests.pbt, na.rm = T),
-                ave.pp.elig.retst=round(mean(eligPP.forTst, na.rm = T),0),
-                pp.tested=sum(tot.tests.PP, na.rm = T),
-                tot.tests.ibt=sum(tot.tests.ibt, na.rm=T), 
-                tot.tested=sum(tot.tests, na.rm=T))
+                incidHIV=sum(incid, na.rm = T),                                 
+                new.diag=sum(recent_diagn, na.rm = T), 
+                nd.elig.for.ps=sum(elig_indexesND,na.rm = T), 
+                nd.initiat.ps=sum(found_indexesND, na.rm = T),
+                ndpart.elicited=sum(elig_partnersND, na.rm = T), 
+                ndpart.found=sum(found_partnersND, na.rm = T), 
+                part.ident.at.psND=sum(tot.part.identND, na.rm = T),
+                ndpart.elig.for.hivtest=sum(elig.part.for.testND, na.rm = T),
+                pbt.doneND=sum(tot.tests.pbtND, na.rm = T),        
+                pbt.pos.testsND=sum(tot.pos.tests.pbtND, na.rm = T))       
+                
+    p_tyrPP<-as.data.frame(df.allsims) %>% mutate(yr=cut(time,breaks = c(seq(0,4*52,by=52)), labels = c("1","2","3","4"),include.lowest = T)) %>% group_by(yr) %>% 
+                summarise(totpop=1000, 
+                #ave.pp.elig.retest.pertimestep=round(mean(eligPP.for.retest, na.rm = T),0),
+                tot.ibt.amongPP=sum(tot.retests.PP, na.rm = T),
+                pp.retests=sum(recent_retests, na.rm = T),               
+                pp.elig.for.ps=sum(elig_indexesPP,na.rm = T), 
+                pp.initiat.ps=sum(found_indexesPP, na.rm = T),
+                pppart.elicited=sum(elig_partnersPP, na.rm = T), 
+                pppart.found=sum(found_partnersPP, na.rm = T), 
+                part.ident.at.psPP=sum(tot.part.identPP, na.rm = T),
+                pppart.elig.for.hivtest=sum(elig.part.for.testPP, na.rm = T),
+                pbt.donePP=sum(tot.tests.pbtPP, na.rm = T),        
+                pbt.pos.testsPP=sum(tot.pos.tests.pbtPP, na.rm = T))
     
-    #Tested for HIV after PS
-    p_tafterPS<-df.allsims %>% filter(time>=1*52) %>% group_by(sim) %>% 
-      summarise(totpop=1000, 
-                incidHIV=sum(incid, na.rm = T), 
-                ndindexes=sum(recent_diagn, na.rm = T), 
-                ndindexes.elig=sum(elig_indexes,na.rm = T), 
-                ndindexes.initPS=sum(found_indexes, na.rm = T),
-                ndpart.elicit=sum(elig_partners, na.rm = T), 
-                ndpart.ident=sum(found_partners, na.rm = T), 
-                ndpart.ident2=sum(tot.part.ident, na.rm=T), 
-                ndpart.eligTst=sum(elig.part.for.pbt, na.rm = T),
-                ndpart.tested=sum(tot.tests.pbt, na.rm = T),
-                ave.pp.elig.retst=round(mean(eligPP.forTst, na.rm = T),0),
-                pp.tested=sum(tot.tests.PP, na.rm = T),
-                tot.tests.ibt=sum(tot.tests.ibt, na.rm=T), 
-                tot.tested=sum(tot.tests, na.rm=T))
+    p_tyrTOT<-as.data.frame(df.allsims) %>% mutate(yr=cut(time,breaks = c(seq(0,4*52,by=52)), labels = c("1","2","3","4"),include.lowest = T)) %>% group_by(yr) %>% 
+      summarise(totpop=1000,     
+                #tot.part.identified.at.ps=sum(tot.part.ident, na.rm = T),
+                tot.nonPP.ibt=sum(tot.tests.ibt, na.rm=T), 
+                tot.pos.nonPP.ibt=sum(tot.pos.tests.ibt, na.rm = T),
+                tot.PP.ibt=sum(tot.retests.PP, na.rm = T),
+                tot.pos.PP.ibt=sum(tot.retests.PP, na.rm = T),
+                tot.pbt=sum(tot.tests.pbtND, na.rm = T)+sum(tot.tests.pbtPP, na.rm = T),
+                tot.pos.pbt=sum(tot.pos.tests.pbtND, na.rm = T) + sum(tot.pos.tests.pbtPP, na.rm = T),
+                tot.tests=sum(tot.tests, na.rm=T),
+                tot.pos.tests=sum(tot.pos.tests.ibt, na.rm = T)+sum(tot.retests.PP, na.rm = T)+sum(tot.pos.tests.pbtND, na.rm = T) + sum(tot.pos.tests.pbtPP, na.rm = T)) %>% 
+      mutate(ppn_pos_tests_arePP=tot.pos.PP.ibt/tot.pos.tests)
     
-    p_t<-data.frame(rbind(b4=p_tb4PS,after=p_tafterPS)) %>% tibble::add_column(time=c("b4","after"), .before="sim") %>% select(-c(sim, totpop));p_t
+    p_tyrND
+    p_tyrPP
+    p_tyrTOT
     
+
   
