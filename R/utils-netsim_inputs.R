ac7b3be1e86cefd0da07fbf39112e842a534741b
UddieEpi <uonwubi@emory.edu>
2022-09-06 20:27:42 -0400

    Added some default parameters

--- R/utils-netsim_inputs.R
+++ R/utils-netsim_inputs.R
@@ -22,12 +22,23 @@ prep_start         <- calibration_length + 52 * 5 + 1
 interv_start       <- prep_start + 52 * 5
 nsteps             <- interv_start + 52 * 10 - 1
 
+
 # Parameters -------------------------------------------------------------------
 csv_params <- paste0("data/input/params-", netsize_string, ".csv")
 df_params <- readr::read_csv(csv_params)
 
 param <- param.net(
   data.frame.params = df_params,
+  prevpos.retest.start = interv_start,
+  part.index.prob        = 0.667,                                                         #Probability that an ND-index case would initiate PS
+  part.ppindex.prob      = 0.667,                                                         #Probability that an PP-index case would initiate PS
+  part.ident.main.window = 24,                                                            #Num of ts that a main partner qualifies for partner identification (default=12wks)
+  part.ident.casl.window = 24,
+  part.ident.ooff.window = 24,
+  part.ident.main.prob   = 0.5,                                                           #Probability that an elicited main partner is identified
+  part.ident.casl.prob   = 0.5,
+  part.ident.ooff.prob   = 0.5,
+  part.hiv.test.rate     = rep(0.84, 3),                                                  #using param from complete case analysis in combprevnet (YR2 study)
   netstats = netstats,
   epistats = epistats,
   riskh.start = prep_start - 53,
