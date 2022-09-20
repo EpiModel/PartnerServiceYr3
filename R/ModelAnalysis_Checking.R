9146df844ca28457aae44516a2634878f61c4d7e
UddieEpi <uonwubi@emory.edu>
2022-09-20 10:09:23 -0400

    Update ModelAnalysis_Checking.R

--- R/ModelAnalysis_Checking.R
+++ R/ModelAnalysis_Checking.R
@@ -5,16 +5,23 @@ sim
 #Epidemic plots
 par(mfrow=c(2,2))
 plot(sim, leg.cex = 0.4)
-plot(sim, y=c("incid.B","incid.H","incid.W"), qnts = F, legend=T, leg.cex = 0.5)
-plot(sim, y=c("prepElig.B","prepElig.H","prepElig.W"), legend=T, leg.cex = 0.5)
-plot(sim, y=c("prepCurr.B","prepCurr.H","prepCurr.W"), legend=T, leg.cex = 0.5)
+plot(sim, y=c("incid.B","incid.H","incid.W"), qnts = F, legend=T, leg.cex = 0.5, main="HIV incidence")
+abline(v=c(52*1,52*2), col="grey84", lty=c(2,2))
+plot(sim, y=c("prepElig.B","prepElig.H","prepElig.W"), legend=T, leg.cex = 0.5, main="PrEP eligibility")
+abline(v=c(52*1,52*2), col="grey84", lty=c(2,2))
+plot(sim, y=c("prepCurr.B","prepCurr.H","prepCurr.W"), legend=T, leg.cex = 0.5, main='Current PrEP use')
+abline(v=c(52*1,52*2), col="grey84", lty=c(2,2))
+
 
 par(mfrow=c(2,2))
-plot(sim, y=c("incid","incid.B","incid.H","incid.W"), qnts = F, legend=T, leg.cex = 0.5, main="incident HIV)")
-plot(sim, y=c("tot.tests","tot.tests.nprep"), legend=T, leg.cex = 0.5, main="Total tests + non-PrEP tests")
-plot(sim, y=c("tot.tests.B","tot.tests.H","tot.tests.W"), legend=T, leg.cex = 0.5, main="Total tests in B,H,W")
-plot(sim, y=c("tot.tests.ibt","tot.tests.pbt"), legend=T, leg.cex = 0.5, main="Total IBT vs PBT")
-
+plot(sim, y=c("incid","incid.B","incid.H","incid.W"), qnts = F, legend=T, leg.cex = 0.5, main="Incident HIV")
+abline(v=c(52*1,52*2), col="grey84", lty=c(2,2))
+plot(sim, y=c("tot.tests","tot.tests.ibtnotPP", "tot.tests.ibtPP", "tot.tests.pbt"), legend=T, leg.cex = 0.5, main="HIV testing by groups")
+abline(v=c(52*1,52*2), col="grey84", lty=c(2,2))
+plot(sim, y=c("tot.tests","tot.tests.ibtnotPP","retested.nic"), legend=T, leg.cex = 0.5, main="Tests by HIV status")
+abline(v=c(52*1,52*2), col="grey84", lty=c(2,2))
+plot(sim, y=c("tot.tests.ibtPP","retested.nic.rxnaive"), legend=T, leg.cex = 0.5, main="Retests among PP and in Tx naive")
+abline(v=c(52*1,52*2), col="grey84", lty=c(2,2))
 
 #Data extraction
 #all sims
