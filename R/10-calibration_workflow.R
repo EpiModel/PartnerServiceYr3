2ade6bd17b43986674ebe9a5d3318771c0f1c894
UddieEpi <uonwubi@emory.edu>
2022-09-22 12:31:51 -0400

    edit scenarios list

--- R/10-calibration_workflow.R
+++ R/10-calibration_workflow.R
@@ -8,7 +8,7 @@ library("EpiModelHPC")
 
 # hpc_configs <- swf_configs_hyak(hpc = "mox", partition = "csde")
 hpc_configs <- swf_configs_rsph(
-  partition = "preemptable",
+  partition = "epimodel",
   mail_user = "uonwubi@emory.edu"
 )
 
@@ -43,19 +43,25 @@ control <- control_msm(
   cumulative.edgelist = TRUE,
   truncate.el.cuml = 0,
   .tracker.list = calibration_trackers, # created in R/utils-targets.R,
-  .checkpoint.dir = cp_dir,
-  .checkpoint.clear = FALSE,
-  .checkpoint.steps = 30 * 52,
+  # .checkpoint.dir = cp_dir,
+  # .checkpoint.clear = FALSE,
+  # .checkpoint.steps = 30 * 52,
   verbose = FALSE,
   raw.output = FALSE
 )
 
 # insert test values here
+# scenarios.df <- tibble(
+#   .scenario.id = c("base", "interv1", "interv2", "both"),
+#   .at = 1,
+#   prevpos.retest.start	= c(Inf, interv_start, Inf, interv_start),
+#   second.genps.start	= c(Inf, Inf, interv_start, interv_start)
+# )
 scenarios.df <- tibble(
-  .scenario.id = c("base", "interv1", "interv2", "both"),
+  .scenario.id = c("base", "both"),
   .at = 1,
-  prevpos.retest.start	= c(Inf, interv_start, Inf, interv_start),
-  second.genps.start	= c(Inf, Inf, interv_start, interv_start)
+  prevpos.retest.start	= c(Inf, interv_start),
+  second.genps.start	= c(Inf, interv_start)
 )
 scenarios.list <- EpiModel::create_scenario_list(scenarios.df)
 
@@ -66,7 +72,7 @@ wf <- add_workflow_step(
     scenarios_list = scenarios.list,
     output_dir = "data/output/calib",
     libraries = "EpiModelHIV",
-    n_rep = 10,                                                                          #num. of simulations per scenario
+    n_rep = 2,                                                                            #num. of simulations per scenario
     n_cores = max_cores,
     max_array_size = 500,
     setup_lines = hpc_configs$r_loader
@@ -79,28 +85,28 @@ wf <- add_workflow_step(
   )
 )
 
-# Process calibrations ---------------------------------------------------------
-# produce a data frame with the calibration targets for each scenario
-wf <- add_workflow_step(
-  wf_summary = wf,
-  step_tmpl = step_tmpl_do_call_script(
-    r_script = "R/12-calibration_process.R",
-    args = list(
-      ncores = 15,
-      nsteps = 52,
-      cp_dir = cp_dir
-    ),
-    setup_lines = hpc_configs$r_loader
-  ),
-  sbatch_opts = list(
-    "cpus-per-task" = max_cores,
-    "time" = "04:00:00",
-    "mem-per-cpu" = "4G",
-    "mail-type" = "END"
-  )
-)
+# # Process calibrations ---------------------------------------------------------
+# # produce a data frame with the calibration targets for each scenario
+# wf <- add_workflow_step(
+#   wf_summary = wf,
+#   step_tmpl = step_tmpl_do_call_script(
+#     r_script = "R/12-calibration_process.R",
+#     args = list(
+#       ncores = 15,
+#       nsteps = 52,
+#       cp_dir = cp_dir
+#     ),
+#     setup_lines = hpc_configs$r_loader
+#   ),
+#   sbatch_opts = list(
+#     "cpus-per-task" = max_cores,
+#     "time" = "04:00:00",
+#     "mem-per-cpu" = "4G",
+#     "mail-type" = "END"
+#   )
+# )
 # to send the workflows on the HPC
-  # Mine (copy and run in R terminal)
+  # Mine (Run in R terminal)
   # scp -r workflows/calibration sph:/projects/epimodel/uonwubi/PartnerServiceYr3/workflows/calibration
 
 # to execute the workflow (do on the HPC *****Windows users have to run both code lines****)
