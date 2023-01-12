1ed82c34a58d38b967484f676e6fe1bbf8f98bff
UddieEpi <uonwubi@emory.edu>
2023-01-12 05:47:18 -0500

    updated utils_trackers to match calib model version

--- R/utils-epi_trackers.R
+++ R/utils-epi_trackers.R
@@ -1,132 +1,151 @@
+library("EpiModel")
+
 # Utilities --------------------------------------------------------------------
-# makes the trackers race specific
-# indiv == TRUE: 3 trackers: tracker___B, tracker___H, tracker___W
-# full == TRUE: add one trackers: tracker___ALL
-epi_tracker_by_race <- function(ls_funs, races = 1:3,
-                                races_names = c("B", "H", "W"),
-                                indiv = TRUE, full = TRUE) {
 
- ls_races <- if (indiv) as.list(races) else list()
- races_names <- if (indiv) races_names else c()
-
-  if (full) {
-    ls_races <- c(ls_races, list(races))
+#' Takes a list of epi_trackers factory and return a list of epi_trackers
+#'
+#' This function accept epi_tracers factories as input. These are function
+#' with one argument `races_set` that return an epi_tracker specific to a set of
+#' races.
+#' This function is used when the same trackers are used for different races
+#'
+#' @param trackers_list a list of epi_trackers factories
+#' @param races the races of interest using the internal identifiers (here
+#'   integers)
+#' @param races_names character names to identify the races (here B, H W)
+#' @param individual_trackers should a tracker be created for each of the
+#'   `races`? (default = TRUE)
+#' @param global_trackers should a tracker be created for the population as a
+#'   whole, i.e. not stratified by race? (default = TRUE)
+epi_trackers_by_races <- function(trackers_list,
+                                  races = c(1, 2, 3),
+                                  races_names = c("B", "H", "W"),
+                                  individual_trackers = TRUE,
+                                  global_trackers = TRUE) {
+  races_list <- if (individual_trackers) as.list(races) else list()
+  races_names <- if (individual_trackers) races_names else c()
+  
+  if (global_trackers) {
+    races_list <- c(races_list, list(races))
     races_names <- c(races_names, "ALL")
   }
-
-  epi_tracker <- lapply(
-    ls_races,
-    function(race) lapply(ls_funs, do.call, args = list(r_ind = race))
+  
+  epi_trackers <- lapply(
+    races_list,
+    function(races) {
+      lapply(trackers_list, do.call, args = list(races_set = races))
+    }
   )
-
-  epi_tracker <- unlist(epi_tracker)
-  names(epi_tracker) <- paste0(
-    names(epi_tracker), "___",
-    unlist(lapply(races_names, rep, times = length(ls_funs)))
+  
+  epi_trackers <- unlist(epi_trackers)
+  names(epi_trackers) <- paste0(
+    names(epi_trackers), "___",
+    unlist(lapply(races_names, rep, times = length(trackers_list)))
   )
-
-  epi_tracker
+  
+  return(epi_trackers)
 }
 
 # Trackers ---------------------------------------------------------------------
-epi_n <- function(r_ind) {
+epi_n <- function(races_set) {
   function(dat) {
     needed_attributes <- c("race", "active")
     with(get_attr_list(dat, needed_attributes), {
-      sum(race %in% r_ind & active == 1, na.rm = TRUE)
+      sum(race %in% races_set & active == 1, na.rm = TRUE)
     })
   }
 }
 
 # HIV Trackers
-epi_s <- function(r_ind) {
+epi_s <- function(races_set) {
   function(dat) {
     needed_attributes <- c("race", "status")
     with(get_attr_list(dat, needed_attributes), {
-      sum(race %in% r_ind & status == 0, na.rm = TRUE)
+      sum(race %in% races_set & status == 0, na.rm = TRUE)
     })
   }
 }
 
 # eligible to prep
-epi_s_prep_elig <- function(r_ind) {
+epi_s_prep_elig <- function(races_set) {
   function(dat) {
     needed_attributes <- c("race", "status", "prepElig")
     with(get_attr_list(dat, needed_attributes), {
-      sum(race %in% r_ind & status == 0 & prepElig == 1, na.rm = TRUE)
+      sum(race %in% races_set & status == 0 & prepElig == 1, na.rm = TRUE)
     })
   }
 }
 
 # on prep
-epi_s_prep <- function(r_ind) {
+epi_s_prep <- function(races_set) {
   function(dat) {
     needed_attributes <- c("race", "status", "prepStat")
     with(get_attr_list(dat, needed_attributes), {
-      sum(race %in% r_ind & status == 0 & prepStat == 1, na.rm = TRUE)
+      sum(race %in% races_set & status == 0 & prepStat == 1, na.rm = TRUE)
     })
   }
 }
 
-epi_i <- function(r_ind) {
+epi_i <- function(races_set) {
   function(dat) {
     needed_attributes <- c("race", "status")
     with(get_attr_list(dat, needed_attributes), {
-      sum(race %in% r_ind & status == 1, na.rm = TRUE)
+      sum(race %in% races_set & status == 1, na.rm = TRUE)
     })
   }
 }
 
-epi_i_dx <- function(r_ind) {
+epi_i_dx <- function(races_set) {
   function(dat) {
     needed_attributes <- c("race", "status", "diag.status")
     with(get_attr_list(dat, needed_attributes), {
-      sum(race %in% r_ind & status == 1 & diag.status == 1, na.rm = TRUE)
+      sum(race %in% races_set & status == 1 & diag.status == 1, na.rm = TRUE)
     })
   }
 }
 
-epi_i_tx <- function(r_ind) {
+epi_i_tx <- function(races_set) {
   function(dat) {
     needed_attributes <- c("race", "status", "tx.status")
     with(get_attr_list(dat, needed_attributes), {
-      sum(race %in% r_ind & status == 1 & tx.status == 1, na.rm = TRUE)
+      sum(race %in% races_set & status == 1 & tx.status == 1, na.rm = TRUE)
     })
   }
 }
 
-epi_i_sup <- function(r_ind) {
+epi_i_sup <- function(races_set) {
   function(dat) {
     at <- get_current_timestep(dat)
     needed_attributes <- c("race", "status", "vl.last.supp")
     with(get_attr_list(dat, needed_attributes), {
-      sum(race %in% r_ind & status == 1 & vl.last.supp == at, na.rm = TRUE)
+      sum(race %in% races_set & status == 1 & vl.last.supp == at, na.rm = TRUE)
     })
   }
 }
 
-epi_i_sup_dur <- function(r_ind) {
+epi_i_sup_dur <- function(races_set) {
   function(dat) {
     at <- get_current_timestep(dat)
     needed_attributes <- c("race", "status", "vl.last.usupp")
     with(get_attr_list(dat, needed_attributes), {
-      sum(race %in% r_ind &
-          status == 1 &
-          at - vl.last.usupp >= 52,
-          na.rm = TRUE)
+      sum(race %in% races_set &
+            status == 1 &
+            at - vl.last.usupp >= 52,
+          na.rm = TRUE
+      )
     })
   }
 }
 
 # linked in less than `weeks` step
 epi_linked_time <- function(weeks) {
-  function(r_ind) {
+  function(races_set) {
     function(dat) {
       needed_attributes <- c("race", "tx.init.time", "diag.time")
       with(get_attr_list(dat, needed_attributes), {
         sum(
-          race %in% r_ind &
-          tx.init.time - diag.time <= weeks,
+          race %in% races_set &
+            tx.init.time - diag.time <= weeks,
           na.rm = TRUE
         )
       })
@@ -136,14 +155,14 @@ epi_linked_time <- function(weeks) {
 
 # STI trackers
 epi_gc_i <- function(hiv_status) {
-  function(r_ind) {
+  function(races_set) {
     function(dat) {
       needed_attributes <- c("race", "rGC", "uGC", "status")
       with(get_attr_list(dat, needed_attributes), {
         sum(
-          race %in% r_ind &
-          status %in% hiv_status &
-          (rGC == 1 | uGC == 1),
+          race %in% races_set &
+            status %in% hiv_status &
+            (rGC == 1 | uGC == 1),
           na.rm = TRUE
         )
       })
@@ -152,14 +171,14 @@ epi_gc_i <- function(hiv_status) {
 }
 
 epi_ct_i <- function(hiv_status) {
-  function(r_ind) {
+  function(races_set) {
     function(dat) {
       needed_attributes <- c("race", "rCT", "uCT", "status")
       with(get_attr_list(dat, needed_attributes), {
         sum(
-          race %in% r_ind &
-          status %in% hiv_status &
-          (rCT == 1 | uCT == 1),
+          race %in% races_set &
+            status %in% hiv_status &
+            (rCT == 1 | uCT == 1),
           na.rm = TRUE
         )
       })
@@ -168,14 +187,14 @@ epi_ct_i <- function(hiv_status) {
 }
 
 epi_gc_s <- function(hiv_status) {
-  function(r_ind) {
+  function(races_set) {
     function(dat) {
       needed_attributes <- c("race", "rGC", "uGC", "status")
       with(get_attr_list(dat, needed_attributes), {
         sum(
-          race %in% r_ind &
-          status %in% hiv_status &
-          (rGC == 0 & uGC == 0),
+          race %in% races_set &
+            status %in% hiv_status &
+            (rGC == 0 & uGC == 0),
           na.rm = TRUE
         )
       })
@@ -184,14 +203,30 @@ epi_gc_s <- function(hiv_status) {
 }
 
 epi_ct_s <- function(hiv_status) {
-  function(r_ind) {
+  function(races_set) {
     function(dat) {
       needed_attributes <- c("race", "rCT", "uCT", "status")
       with(get_attr_list(dat, needed_attributes), {
         sum(
-          race %in% r_ind &
-          status %in% hiv_status &
-          (rCT == 0 & uCT == 0),
+          race %in% races_set &
+            status %in% hiv_status &
+            (rCT == 0 & uCT == 0),
+          na.rm = TRUE
+        )
+      })
+    }
+  }
+}
+
+epi_prep_ret <- function(ret_steps) {
+  function(races_set) {
+    function(dat) {
+      at <- get_current_timestep(dat)
+      needed_attributes <- c("race", "prepStartTime")
+      with(get_attr_list(dat, needed_attributes), {
+        retained <- sum(
+          race %in% races_set &
+            prepStartTime == at - ret_steps,
           na.rm = TRUE
         )
       })
