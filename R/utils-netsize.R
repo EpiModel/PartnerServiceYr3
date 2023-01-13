041134ebeae2e9cf15a6943129a789ad5babfa61
UddieEpi <uonwubi@emory.edu>
2023-01-13 15:11:56 -0500

    edit

--- R/utils-netsize.R
+++ R/utils-netsize.R
@@ -5,9 +5,22 @@
 ##  different scripts
 ##
 
-# Size of network to use
-if (! exists("NETSIZE")) {
-  NETSIZE <- 100 * 1e3
-}
+# # Size of network to use
+# if (! exists("NETSIZE")) {
+#   NETSIZE <- 100 * 1e3
+# }
 
+if (context == "hpc") {
+  networks_size   <- 100 * 1e3
+  NETSIZE <- 100 *1e3
+  estimation_method <- "MCMLE"
+  estimation_ncores <- 10
+} else if (context == "local") {
+  networks_size   <- 10 * 1e3
+  NETSIZE <- 10 *1e3
+  estimation_method <- "Stochastic-Approximation"
+  estimation_ncores <- 1
+} else {
+  stop("The `context` variable must be set to either 'local' or 'hpc'")
+}
 netsize_string <- format(NETSIZE, scientific = FALSE)
