60317de58fbc063543187df3094fb2c3bb4616d7
UddieEpi <uonwubi@emory.edu>
2023-01-13 19:19:14 -0500

    process data for tables

--- R/41-intervention_scenarios_process.R
+++ R/41-intervention_scenarios_process.R
@@ -1,45 +1,61 @@
 ##
 ## 11. Epidemic Model Parameter Calibration, Processing of the simulation files
 ##
-
+#context<-"local"
 # Setup ------------------------------------------------------------------------
 #source("R/utils-0_project_settings.R")
 source("R/utils-scenarios_outcomes.R")
+source("R/utils-netsim_inputs.R")
+source("R/utils-netsize.R")
 
+#get batch info
 batches_infos <- EpiModelHPC::get_scenarios_batches_infos(
   "data/intermediate/scenarios"
 )
 
-# process each batch
+
+
+#process each batch of simulation in parallel
 outcomes_raw <- lapply(
   seq_len(nrow(batches_infos)),
   function(i) process_one_scenario_batch(batches_infos[i, ])
 )
 
-# bind all rows into 1 data frame with 1 row per unique simulation
-outcomes_raw <- bind_rows(outcomes_raw)
-head(outcomes_raw)
-saveRDS(outcomes_raw, "data/intermediate/scenarios/outcomes_raw.rds")
 
-# summarise the results into a data frame with one row per scenario
-#   here we present each outcome with q1, median, q3
-outcomes <- outcomes_raw %>%
+
+#bind all batches (and simulation rows) into 1 data frame 
+#output=all scenarios with 1 row per unique simulation
+outcomes_sims <- bind_rows(outcomes_raw) %>%  
+  group_by(scenario_name) %>%    #re-number sims for each scenario
+  mutate(sim2 = sim) %>% 
+  mutate(sim = ifelse(batch_number > 1, sim2 + ((batch_number-1) * max_cores), sim2)) %>% 
+  select(-sim2)  %>% 
+  #rename scenarios
+  mutate(scenario.new = ifelse(scenario_name == "base","Base",
+                             ifelse(scenario_name == "interv1","+ PP retests",
+                                    ifelse(scenario_name == "interv2","+ Wave 2 PS","+ Both")))) %>% 
+  mutate(scenario.new = factor(scenario.new, levels = c("Base","+ PP retests","+ Wave 2 PS","+ Both"))) %>% 
+  arrange(scenario.new)
+saveRDS(outcomes_sims, "data/intermediate/processed/outcomes_sims.rds")
+
+
+
+#summarise over simulations per scenario using median, 50% SI
+outcomes_scenarios <- outcomes_sims %>%
   select(- c(sim, batch_number)) %>%
-  group_by(scenario_name) %>%
-  summarise(across(
-    everything(),
-    list(
-      q1 = ~ quantile(.x, 0.25, na.rm = TRUE),
-      q2 = ~ quantile(.x, 0.50, na.rm = TRUE),
-      q3 = ~ quantile(.x, 0.75, na.rm = TRUE)
+  group_by(scenario_name, scenario.new) %>%
+  summarise(across(everything(),list(
+      low = ~ quantile(.x, 0.25, na.rm = TRUE),
+      med = ~ quantile(.x, 0.50, na.rm = TRUE),
+      high = ~ quantile(.x, 0.75, na.rm = TRUE)
     ),
     .names = "{.col}__{.fn}"
-  ))
+  )) %>% ungroup()%>% arrange(scenario.new)
 
-head(outcomes)
+
 
 # Save the result --------------------------------------------------------------
-saveRDS(outcomes, "data/intermediate/scenarios/outcomes.rds")
-readr::write_csv(outcomes, "data/intermediate/scenarios/outcomes.csv")
+saveRDS(outcomes_scenarios_med95si, "data/intermediate/processed/outcomes_scenarios.rds")
+readr::write_csv(outcomes_scenarios_med95si, "data/intermediate/processed/outcomes_scenarios.csv")
 
 
