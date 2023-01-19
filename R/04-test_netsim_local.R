674e4dab67b1b6c8f6a5264ddfb5a21561c834d9
UddieEpi <uonwubi@emory.edu>
2023-01-19 11:31:15 -0500

    edit local netsim script for scenarios

--- R/04-test_netsim_local.R
+++ R/04-test_netsim_local.R
@@ -5,13 +5,16 @@
 # ----------------------------------------------------------------------------------------
 
 ## Example interactive epidemic simulation run script with basic parameterization
-##    and all parameters defined in `param_msm`, with example of writing/debugging modules
-
-#source("R/utils-0_project_settings.R")
-context <- "local"
+## and all parameters defined in `param_msm`, with example of writing/debugging modules
 
 #Libraries
 library("EpiModelHIV")
+
+context <- "local"
+
+#source("R/utils-0_project_settings.R")
+source("R/utils-netsim_inputs.R")
+source("R/utils-netsize.R")
 
 
 #Set up files 
@@ -98,99 +101,100 @@ context <- "local"
 #Libraries
 library("EpiModelHIV")
 
+  
+  
+  
 #Set up files 
-#~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 source("R/utils-netsim_inputs.R") 
 source("R/utils-targets.R")
 source("R/utils-netsize.R")
 
-  # epistats <- readRDS("data/input/epistats.rds")
-  # netstats <- readRDS(paste0("data/input/netstats-", netsize_string, ".rds"))
-  # est <- readRDS(paste0("data/input/netest-", netsize_string, ".rds"))
   
+#epistats, netstats and netest
 epistats <- readRDS("data/intermediate/estimates/epistats-local.rds")
 netstats <- readRDS("data/intermediate/estimates/netstats-local.rds")
 est      <- readRDS("data/intermediate/estimates/netest-local.rds")
 
+
+#controls
+control <- control_msm(
+  start               = restart_time,
+  nsteps              = nsteps,
+  nsims               = 1,
+  ncores              = 1,
+  initialize.FUN      = reinit_msm,
+  cumulative.edgelist = TRUE,
+  truncate.el.cuml    = 0,
+  .tracker.list       = calibration_trackers,
+  verbose             = TRUE)
+
+
+#scenarios
+scenarios_df <- tibble::tibble(
+  .scenario.id = c("base","interv1","interv2", "both"),
+  .at = 1,
+  prevpos.retest.start	= c(Inf,interv_start, Inf, interv_start),
+  second.genps.start	= c(Inf,Inf, interv_start, interv_start)
+)
+scenarios_list <- EpiModel::create_scenario_list(scenarios_df)
   
-  #edit times
-  calibration_length <- 52 * 0
-  restart_time       <- calibration_length + 1                       
-  prep_start         <- restart_time + (52 * 0)
-  interv_start       <- prep_start + (52 * 0.25)                       
-  nsteps             <- interv_start + (52 * .25)
-  
-  #controls
-  control <- control_msm(
-    start               = restart_time,
-    nsteps              = nsteps,
-    nsims               = 1,
-    ncores              = 1,
-    initialize.FUN      = reinit_msm,
-    cumulative.edgelist = TRUE,
-    truncate.el.cuml    = 0,
-    .tracker.list       = calibration_trackers,
-    verbose             = TRUE)
-  
-  #set up scenarios
-  scenarios_df <- tibble::tibble(
-    .scenario.id = c("base","interv1"),#, "interv2", "both"),
-    .at = 1,
-    prevpos.retest.start	= c(interv_start,interv_start),#, Inf, interv_start),
-    second.genps.start	= c(Inf,Inf)#, interv_start), interv_start)
-  )
-  scenarios_list <- EpiModel::create_scenario_list(scenarios_df)
-  
+
+
+
+
 #Simulate epidemic in scenarios and examine output 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  
-    #Method 1
-      #expect 4 files (2 per scenario)
-      EpiModelHPC::netsim_scenarios(
-        path_to_restart, param, init, control, scenarios_list,
-        n_rep = 3,
-        n_cores = 2,
-        output_dir = "data/intermediate/scenarios",
-        libraries = "EpiModelHIV",
-        save_pattern = "simple"
-      )
-      list.files("data/intermediate/scenarios")
+#Method 1
+  #run scenarios as on the hpc to output sims of intervention scenarios in batches.
+  #n_rep=3 and n_cores=2: expect 4 files (2 per scenario)
+  EpiModelHPC::netsim_scenarios(
+    path_to_restart, param, init, control, scenarios_list,
+    n_rep = 3,
+    n_cores = 2,
+    output_dir = paste0("data/intermediate/",context,"/scenarios"),
+    libraries = "EpiModelHIV",
+    save_pattern = "simple"
+  )
+  list.files(paste0("data/intermediate/",context,"/scenarios"))
  
       
  
-    #Method 2 (use to debug in scenario testing)
-      options(error = recover)
-      
-      #set up a list to hold the sim results
-        d_list <- vector(mode = "list",
-                       length = length(scenarios_list))
-        names(d_list) <- names(scenarios_list)
-      
-      #reload EMHIV-p package from local (if changes made locally)
-        pkgload::load_all("C:/Users/Uonwubi/OneDrive - Emory University/Desktop/Personal/RSPH EPI Docs/RA2/GitRepos/EpiModelHIV-p")
-      
-        control <- control_msm(
-          simno = 1,
-          nsteps = nsteps,
-          nsims = 1,
-          ncores = 1,
-          verbose = TRUE)
+#Method 2 (use to debug in scenario testing)
+  #slist to hold the sim results
+  d_list <- vector(mode = "list", length = length(scenarios_list))
+  names(d_list) <- names(scenarios_list)
+
+#reload EMHIV-p package from local (if changes made locally)
+  pkgload::load_all("C:/Users/Uonwubi/OneDrive - Emory University/Desktop/Personal/RSPH EPI Docs/RA2/GitRepos/EpiModelHIV-p")
+
+  control <- control_msm(
+    simno = 1,
+    nsteps = nsteps,
+    nsims = 1,
+    ncores = 1,
+    verbose = TRUE)
+
+#run netsim() looped over the scenarios
+  # debug(partident_msm)
+  # debug(hivtest_msm)
+  # debug(prep_msm)
+  # debug(hivtx_msm)
+  #options(error = recover)
+  for (scenario in scenarios_list){
+    print(scenario$id)
+    sc.param <- use_scenario(param, scenario)
+    sim <- netsim(est, sc.param, init, control)
     
-      #run netsim() looped over the scenarios
-        #debug(hivtest_msm)
-        #debug(partident_msm)
-        #debug(hivtx_msm)
-        options(error = recover)
-        for (scenario in scenarios_list){
-          print(scenario$id)
-          sc.param <- use_scenario(param, scenario)
-          sim <- netsim(est, sc.param, init, control)
-      
-          #convert sim object to a df
-          d_sim <- as.data.frame(sim)
-          d_sim[["scenario"]] <- scenario$id
-          d_list[[scenario$id]] <-d_sim
-        }
-        # undebug(hivtest_msm)
-        # undebug(partident_msm)
-        # undebug(hivtx_msm)
+    saveRDS(sim, paste0("data/intermediate/",context,"/scenarios/sim__",scenario$id,".rds"))
+
+    #convert sim object to a df
+    d_sim <- as.data.frame(sim)
+    d_sim[["scenario"]] <- scenario$id
+    d_list[[scenario$id]] <-d_sim
+  }
+  # undebug(hivtest_msm)
+  # undebug(partident_msm)
+  # undebug(hivtx_msm)
+  # undebug(prep_msm)
+  
