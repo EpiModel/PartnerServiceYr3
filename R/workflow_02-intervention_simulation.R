e0e50727072582619ba7ff6486cae72892f26120
UddieEpi <uonwubi@emory.edu>
2023-02-12 12:27:52 -0500

    increase num sims to 500

--- R/workflow_02-intervention_simulation.R
+++ R/workflow_02-intervention_simulation.R
@@ -95,7 +95,7 @@ wf <- add_workflow_step(
     output_dir = "data/intermediate/hpc/scenarios",
     libraries = "EpiModelHIV",
     save_pattern = "simple",
-    n_rep = 320,                                                                            
+    n_rep = 500,                                                                            
     n_cores = max_cores,
     max_array_size = 999,
     setup_lines = hpc_configs$r_loader
