dcb0c525e8ea4f136af4b3ab63ac2d1dbd86d91d
UddieEpi <uonwubi@emory.edu>
2023-04-18 20:25:28 -0400

    New processing script for paper output tables

---
+++ R/42-paper_counterfactuals_process.R
@@ -0,0 +1,81 @@
+##
+## 42. Processing counterfactual scenarios
+##
+context<-"hpc"
+
+# Setup ------------------------------------------------------------------------
+#get context and scenarios functions
+source("R/utils-netsim_inputs.R")
+source("R/utils-netsize.R")
+source("R/utils-paper_output_functions.R")
+
+
+
+#get batch info
+batches_infos <- EpiModelHPC::get_scenarios_batches_infos(
+  paste0("data/intermediate/",context,"/scenarios")
+)
+
+
+
+
+#A. Process intervdata ------------------------------------------------------------------------
+install.packages("future.apply")
+suppressMessages({
+  library("EpiModelHIV")
+  library("future.apply")
+  library("tidyr")
+  library("dplyr")
+  library("ggplot2")
+})
+
+sims_dir <- paste0("data/intermediate/",context,"/scenarios")
+
+
+#get sim files
+sim_files <- list.files(
+  sims_dir,
+  pattern = "^sim__.*rds$",
+  full.names = TRUE
+)
+
+
+#Process each batch in parallel 
+intervds <- future.apply::future_lapply(
+  sim_files,
+  process_fulldata,
+  ts = interv_start - (5 * 52) + 1   #gets data from 5 years prior to intervention start
+)
+
+
+#Merge all batches  
+full_intervdata <- bind_rows(intervds)
+
+
+
+#B. Process outcome_sims and outcome_scenario data----------------------------------------
+outcomes_sims <- get_outcome_sims(full_intervdata)
+outcomes_scenarios <- outcomes_sims %>%
+  select(- c(sim)) %>%
+  group_by(scenario_name, scenario.new) %>%
+  summarise(across(everything(),list(
+    low = ~ quantile(.x, 0.025, na.rm = TRUE),
+    med = ~ quantile(.x, 0.50, na.rm = TRUE),
+    high = ~ quantile(.x, 0.975, na.rm = TRUE)
+  ),
+  .names = "{.col}__{.fn}"
+  )) %>% 
+  mutate(across(where(is.numeric), ~round (., 5))) %>% ungroup()%>% arrange(scenario.new)
+
+
+
+
+#Save the processed data
+saveRDS(outcomes_sims, paste0("data/intermediate/",context,"/processed/paper_outcomes_sims.rds"))
+saveRDS(outcomes_scenarios, paste0("data/intermediate/",context,"/processed/paper_outcomes_scenarios.rds"))
+#readr::write_csv(outcomes_scenarios_med95si, "data/intermediate/processed/outcomes_scenarios.csv")
+saveRDS(full_intervdata, paste0("data/intermediate/",context,"/processed/paper_fulldata.rds"))
+
+
+
+
