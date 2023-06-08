1154b58a213b626dadf4fe019e7730257247a00f
UddieEpi <uonwubi@emory.edu>
2023-06-08 06:26:51 -0400

    add log files removal

--- R/44.2-fig_simsremove.R
+++ R/44.2-fig_simsremove.R
@@ -3,9 +3,19 @@
 ##
 
 
-#get sims directory
+#remove sim files
 sims_dir <- paste0("data/intermediate/hpc/figdata")
 
-files_to_delete <- dir(path = sims_dir, pattern = "^sim__.*rds$")
+files_to_delete_1 <- dir(path = sims_dir, pattern = "^sim__.*rds$")
 
-file.remove(file.path(sims.dir, files_to_delete))
+file.remove(file.path(sims_dir, files_to_delete_1))
+
+
+
+
+#remove log files
+log_dir <- paste0("workflows/psy3fitdat/log")
+
+files_to_delete_2 <- dir(path = log_dir, pattern = "^psy3figdat_step.*out$")
+
+file.remove(file.path(log_dir, files_to_delete_2))
