d64977f1c6274a2727f7a03073f44ac23f8270d9
UddieEpi <uonwubi@emory.edu>
2023-06-23 09:38:39 -0400

    update trackers

--- R/43-tbl3_dataprocessing.R
+++ R/43-tbl3_dataprocessing.R
@@ -59,12 +59,28 @@ outcomes_sims_tbl3 <- get_outcome_sims_tbl3(full_intervdata_tbl3) %>%
          ir.yr10, incid.cum, nia, pia, nnt,
          prepCov.yr10, diagCov.yr10, artCov.yr10, vSuppCov.yr10,
          prepStartAll,
+         
+         elig.indexes.nd, found.indexes.nd, prp.indexes.found.nd,
+         elig.indexes.pp, found.indexes.pp, prp.indexes.found.pp,
          elig.indexes.all, found.indexes.all, prp.indexes.found.all,
+         
+         elig.partners, found.partners, prp.partners.found.gen1, posPart.indexes, negunkPart.indexes, 
+         elig.partners.gen2, found.partners.gen2, prp.partners.found.gen2,
          elig.partners.all, found.partners.all, prp.partners.found.all,
-         partners.per.index, 
-         tot.tests.pbt, positive.part, negative.part,negative.part2,
-         part.scrnd.tst, part.scrnd.prep, scrnd.prepon, scrnd.noprep, scrnd.noprepnorisk,
-         elig.prepStartPart, prepStartPart, part.start.tx, part.reinit.tx, pp.reinit.tx)
+         
+         partners.per.index,
+         
+         tot.part.ident, elig.for.scrn,
+         tot.tests.pbt, part.scrnd.tst, positive.part, negative.part, 
+         
+         part.scrnd.prep, scrnd.prepon, scrnd.noprep, scrnd.noprepnorisk,
+         elig.prepStartPart, prepStartPart,
+         
+         part.start.tx,
+         part.reinit.tx,
+         gen.start.tx,
+         pp.reinit.tx)
+
 
 
 outcomes_scenarios_tbl3 <- outcomes_sims_tbl3 %>%
