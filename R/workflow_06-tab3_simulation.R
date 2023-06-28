8aefe8e3a36f6eb700028fb8ac6ed44e74cbb078
UddieEpi <uonwubi@emory.edu>
2023-06-28 11:23:15 -0400

    Update workflow_06-tab3_simulation.R

--- R/workflow_06-tab3_simulation.R
+++ R/workflow_06-tab3_simulation.R
@@ -347,7 +347,7 @@ wf <- add_workflow_step(
 # scp -r data/intermediate/hpc/estimates sph:projects/epimodel/uonwubi/PartnerServiceYr3/data/intermediate/hpc/
   
 # to send workflows to the HPC (Run in R terminal)
-# scp -r workflows/psy3sim_tbl3 sph:projects/epimodel/uonwubi/PartnerServiceYr3/workflows
+# scp -r workflows/psy3tbl3all sph:projects/epimodel/uonwubi/PartnerServiceYr3/workflows
 
 # to get the data back after simulations (Run in R terminal)
 # scp -r sph:/projects/epimodel/uonwubi/PartnerServiceYr3/data/intermediate/hpc data/intermediate
