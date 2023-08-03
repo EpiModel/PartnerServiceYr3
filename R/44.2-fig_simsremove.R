40356a89bebb5b4f6423826e86d6282d0a69d9ad
UddieEpi <uonwubi@emory.edu>
2023-08-03 13:39:33 -0400

    Update 44.2-fig_simsremove.R

--- R/44.2-fig_simsremove.R
+++ R/44.2-fig_simsremove.R
@@ -7,7 +7,6 @@
 sims_dir <- paste0("data/intermediate/hpc/figdata")
 
 files_to_delete_1 <- dir(path = sims_dir, pattern = "^sim__.*rds$")
-
 file.remove(file.path(sims_dir, files_to_delete_1))
 
 
@@ -17,5 +16,4 @@ file.remove(file.path(sims_dir, files_to_delete_1))
 log_dir <- paste0("workflows/psy3fitdat/log")
 
 files_to_delete_2 <- dir(path = log_dir, pattern = "^psy3figdat_step.*out$")
-
 file.remove(file.path(log_dir, files_to_delete_2))
