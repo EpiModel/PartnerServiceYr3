29cb38b97f0db829db8ecf346f1dd49f9dc25b42
UddieEpi <uonwubi@emory.edu>
2023-08-05 17:09:42 -0400

    Update 74.2-contourfig_removefiles.R

--- R/74.2-contourfig_removefiles.R
+++ R/74.2-contourfig_removefiles.R
@@ -15,5 +15,9 @@ file.remove(file.path(sims_dir, files_to_delete_1))
 #remove log files
 log_dir <- paste0("workflows/psy3fitdat/log")
 
-files_to_delete_2 <- dir(path = log_dir, pattern = "^psy3figdat_step.*out$")
+files_to_delete_2 <- dir(path = log_dir, pattern %in% c("^psy3figdat_step2.*out$", 
+                                                        "^psy3figdat_step5.*out$",
+                                                        "^psy3figdat_step8.*out$",
+                                                        "^psy3figdat_step11.*out$",
+                                                        "^psy3figdat_step14.*out$"))
 file.remove(file.path(log_dir, files_to_delete_2))
