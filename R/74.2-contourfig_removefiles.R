0208308cc322755a0a0e2b8e26c0ed39b6b5e224
UddieEpi <uonwubi@emory.edu>
2023-08-07 19:40:30 -0400

    Update 74.2-contourfig_removefiles.R

--- R/74.2-contourfig_removefiles.R
+++ R/74.2-contourfig_removefiles.R
@@ -15,26 +15,26 @@ file.remove(file.path(sims_dir, files_to_delete_1))
 #remove log files
 log_dir <- paste0("workflows/psy3fitdat/log")
 
-files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step2.*out$")
+files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step3.*out$")
 file.remove(file.path(log_dir, files_to_delete))
 
-files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step5.*out$")
+files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step4.*out$")
 file.remove(file.path(log_dir, files_to_delete))
 
-files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step8.*out$")
+files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step7.*out$")
 file.remove(file.path(log_dir, files_to_delete))
 
-files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step11.*out$")
+files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step10.*out$")
 file.remove(file.path(log_dir, files_to_delete))
 
-files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step14.*out$")
+files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step13.*out$")
 file.remove(file.path(log_dir, files_to_delete))
 
-files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step17.*out$")
+files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step16.*out$")
 file.remove(file.path(log_dir, files_to_delete))
 
-files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step20.*out$")
+files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step19.*out$")
 file.remove(file.path(log_dir, files_to_delete))
 
-files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step23.*out$")
+files_to_delete <- dir(path = log_dir, pattern = "^psy3figdat_step22.*out$")
 file.remove(file.path(log_dir, files_to_delete))
